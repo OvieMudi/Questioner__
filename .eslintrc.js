1630f65c2ca4f56872b6c2a5c8766f262184f7dc
ovieMudi <oviisaacs@gmail.com>
2018-12-30 06:02:48 +0100

    Change folder structure and modify package.json scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,12 @@ module.exports = {
     "extends": "airbnb-base",
     "env": {
       "browser": true,
+      "mocha": true,
     },
     "rules":{
       "linebreak-style": 0,
+      "no-underscore-dangle": [
+        "error", { "allowAfterThis": true }
+      ]
     },
 };
