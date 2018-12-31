25406c04ea0a7eb07a1f2c7d04441fcea4fd5396
ovieMudi <oviisaacs@gmail.com>
2018-12-31 10:53:30 +0100

    Create a test.js file for running all tests

---
+++ test/api.v1/users.spec.js
@@ -0,0 +1,99 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../server/index';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const data = {
+  firstname: 'Luke',
+  lastname: 'Skywalker',
+  othername: 'Vaderson',
+  email: 'luke@oldrepublic.com',
+  phoneNumber: '00661234567',
+  username: 'skywalker',
+};
+let id;
+
+function userTests() {
+  describe('POST /api/v1/users', () => {
+    it('should create a new User in db', () => chai.request(app)
+      .post('/api/v1/users')
+      .type('form')
+      .send(data)
+      .then((res) => {
+        // eslint-disable-next-line prefer-destructuring
+        id = res.body.data[res.body.data.length - 1].id;
+        expect(res).to.have.status(201);
+        expect(res.body.data[0]).to.have.property('id');
+        expect(res.body.data[0]).to.have.property('firstname').eql('Luke');
+        expect(res.body.data[0]).to.have.property('lastname').eql('Skywalker');
+        expect(res.body.data[0]).to.have.property('othername').eql('Vaderson');
+        expect(res.body.data[0]).to.have.property('email').eql('luke@oldrepublic.com');
+        expect(res.body.data[0]).to.have.property('phoneNumber').eql('00661234567');
+        expect(res.body.data[0]).to.have.property('username').eql('skywalker');
+        expect(res.body.data[0]).to.have.property('registered');
+        expect(res.body.data[0]).to.have.property('isAdmin').that.is.a('boolean');
+      }));
+  });
+  describe('GET /api/v1/users', () => {
+    it('should get all Users in db', () => chai.request(app)
+      .get('/api/v1/users')
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data[0]).to.have.property('id');
+        expect(res.body.data[0]).to.have.property('firstname');
+        expect(res.body.data[0]).to.have.property('lastname');
+        expect(res.body.data[0]).to.have.property('othername');
+        expect(res.body.data[0]).to.have.property('email');
+        expect(res.body.data[0]).to.have.property('phoneNumber');
+        expect(res.body.data[0]).to.have.property('username');
+        expect(res.body.data[0]).to.have.property('registered');
+        expect(res.body.data[0]).to.have.property('isAdmin');
+      }));
+  });
+  describe('GET /api/v1/users/:id', () => {
+    it('should get information of a User from db', () => chai.request(app)
+      .get(`/api/v1/users/${id}`)
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data[0]).to.have.property('id');
+        expect(res.body.data[0]).to.have.property('firstname');
+        expect(res.body.data[0]).to.have.property('lastname');
+        expect(res.body.data[0]).to.have.property('othername');
+        expect(res.body.data[0]).to.have.property('email');
+        expect(res.body.data[0]).to.have.property('phoneNumber');
+        expect(res.body.data[0]).to.have.property('username');
+        expect(res.body.data[0]).to.have.property('registered');
+        expect(res.body.data[0]).to.have.property('isAdmin');
+      }));
+  });
+  describe('Patch /api/v1/users', () => {
+    it('should update a User record in db', () => chai.request(app)
+      .patch(`/api/v1/users/${id}`)
+      .type('form')
+      .send({ othername: 'Last Jedi Master' })
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data[0]).to.have.property('id');
+        expect(res.body.data[0]).to.have.property('firstname');
+        expect(res.body.data[0]).to.have.property('lastname');
+        expect(res.body.data[0]).to.have.property('othername').eql('Last Jedi Master');
+        expect(res.body.data[0]).to.have.property('email');
+        expect(res.body.data[0]).to.have.property('phoneNumber');
+        expect(res.body.data[0]).to.have.property('username');
+        expect(res.body.data[0]).to.have.property('registered');
+        expect(res.body.data[0]).to.have.property('isAdmin');
+      }));
+  });
+  describe('DELETE /api/v1/users/:id', () => {
+    it('should update User information', () => chai.request(app)
+      .delete(`/api/v1/users/${id}`)
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.eql('user deleted');
+      }));
+  });
+}
+
+export default userTests;
---
+++ test/test.js
@@ -0,0 +1,5 @@
+import userTests from './api.v1/users.spec';
+
+describe('Api routes', () => {
+  userTests();
+});
