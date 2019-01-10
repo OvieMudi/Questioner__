acd00f2e50a07625a0866b8fec27ffb2fd027d35
Mudi <42499892+OvieMudi@users.noreply.github.com>
2019-01-10 12:45:53 +0100

    #163073978 Admin can access a page for adding new meetups (#20)
    * Add responsive input for adming to add new meetup
    
    * Add api routes files to a folder for a more logical  structure
    
    * Update imports to new folder structure
    
    * Add dialog box polyfill as a dependency
    
    * Admin can access page to add new meetup and delete meetup

--- server/api/v1/routes/users.js
+++ server/api/v1/routes/users.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import usersController from './controllers/usersController';
+import usersController from '../controllers/usersController';
 
 const Router = express.Router();
 
 // CREATE, READ_ALL
-Router.route('/api/v1/users')
+Router.route('/')
   .post(usersController.postUser)
   .get(usersController.getUsers);
 
 // READ_ONE, UPDATE, DELETE
-Router.route('/api/v1/users/:id')
+Router.route('/:id')
   .get(usersController.getUser)
   .patch(usersController.updateUser)
   .delete(usersController.deleteUser);
