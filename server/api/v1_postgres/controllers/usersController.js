aba662b2ffd317deab92b9913623c8b754d80637
ovieMudi <oviisaacs@gmail.com>
2019-03-10 20:28:51 +0100

    Add auth controller to handle user creation and signin

--- server/api/v1_postgres/controllers/usersController.js
+++ server/api/v1_postgres/controllers/usersController.js
@@ -1,8 +1,30 @@
 import MainController from './mainController';
 import usersModel from '../models/usersModel';
 
-class UsersController extends MainController {}
+class UsersController extends MainController {
+  constructor(name = 'user', model = usersModel) {
+    super(name, model);
+  }
 
-const usersController = new UsersController('user', usersModel);
+  async update(req, res) {
+    try {
+      const row = await this.model.updateUser(req.user.id, req.body);
+      if (!row) return this.errorResponse(res, 404, 'Please login/register first');
+      return this.successResponse(res, 200, row);
+    } catch (error) {
+      return this.errorResponse(res, 400, error.message);
+    }
+  }
 
-export default usersController;
+  async delete(req, res) {
+    try {
+      const deleted = await this.model.delete(req.user.id);
+      if (!deleted) return this.errorResponse(res, 404, `${this.name} does not exist`);
+      return this.successResponse(res, 200, `${this.name} deleted`);
+    } catch (err) {
+      return this.errorResponse(res, 400, err.message);
+    }
+  }
+}
+
+export default new UsersController();
